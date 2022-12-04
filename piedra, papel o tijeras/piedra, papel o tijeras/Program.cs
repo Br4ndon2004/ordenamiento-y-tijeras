706049aef40947dd5428ecd511e79b6ae079d176
Br4ndon2004 <114429903+Br4ndon2004@users.noreply.github.com>
2022-12-04 01:37:10 -0600

    Add files via upload

---
+++ piedra, papel o tijeras/piedra, papel o tijeras/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Piedra_Papel_Tijera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("elige entre:piedra, papel o tijeras ");
+                string op = Console.ReadLine().ToUpper();
+                if (op == "sal")
+                {
+                    break;
+                }
+                if (op == "PIEDRA" || op == "PAPEL" || op == "TIJERA")
+                {
+                    Random rnd = new Random();
+                    int randoomm = rnd.Next(0, 3);
+                    string[] opc = new string[] { "PIEDRA", "PAPEL", "TIJERA" };
+                    string machine = opc[randoomm];
+                    Console.WriteLine("" + machine);
+                    if (op == machine)
+                    {
+                        Console.WriteLine("iguales");
+                    }
+                    else if (op == "PIEDRA" && machine == "TIJERA")
+                    {
+                        Console.WriteLine("usted gano");
+                    }
+                      else if (op == "PAPEL" && machine == "PIEDRA")
+                    {
+                         Console.WriteLine("usted gano");
+                    }
+                       else if (op == "TIJERA" && machine == "PAPEL")
+                    {
+                          Console.WriteLine("usted gano");
+                    }
+                         else
+
+                        Console.WriteLine("has perdido ));");
+                    { 
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("error, tu eleccion no esta en la lista ):");
+                }
+            }
+
+
+        }
+    }
+}
